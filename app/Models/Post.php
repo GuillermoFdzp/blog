25aa68ec959362d049a329e2e0fff87b6f6b05ca
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2024-10-21 14:20:59 +0200

    Mi primer commit

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'usuario_id',
+        'titulo',
+        'contenido'
+    ];
+
+    public function usuario(){
+        return $this->belongsTo(Usuario::class, 'usuario_id');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\api\UsuarioController;
+use App\Http\Controllers\api\PerfilController;
+use App\Http\Controllers\api\PostController;
+use App\Http\Controllers\api\RolController;
+use Illuminate\Http\Request;
+
+Route::apiResource('usuario',UsuarioController::class);
+
+Route::apiResource('perfil',PerfilController::class);
+
+Route::apiResource('post',PostController::class);
+
+Route::apiResource('rol',RolController::class);
+
+
